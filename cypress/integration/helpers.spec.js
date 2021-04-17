7ec4cbff8bb3662b42e877f7e0ad2f4df49a4399
Felipe Meyer <ffmeyer@gmail.com>
2021-04-17 08:14:34 -0300

    metodos wrap, invoke e window

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -44,4 +44,38 @@ describe('Helpers...', () => {
 
     })  
 
+    it('Its..', () => {
+        const obj = { nome: 'User', idade: 20 }
+        cy.wrap(obj).should('have.property', 'nome', 'User')
+        cy.wrap(obj).its('nome').should('be.equal', 'User')
+        
+        const obj2 = { nome: 'User', idade: 20, endereco: {rua: 'dos bobos'}}
+        cy.wrap(obj2).its('endereco').should('have.property', 'rua')
+        cy.wrap(obj2).its('endereco').its('rua').should('contain', 'bobos')
+        cy.wrap(obj2).its('endereco.rua').should('contain', 'bobos')
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.title().its('length').should('be.equal', 20)
+    })
+    
+    it.only('Invoke', () => {
+        const getValue = () => 1;
+        const soma = (a, b) => a+b;
+        /*por ser uma funcao normal em js, eh necessario o wrap para ser utilizada no cypress
+            fn eh alias para getvalue. 
+            a invocacao da mensagem ocorre realmente em invoke('fn')*/
+        cy.wrap({fn: getValue}).invoke('fn').should('be.equal',1)
+        cy.wrap({fn: soma}).invoke( 'fn', 2, 5 ).should('be.equal',7)
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        /*usando metodo invoke para buscar objetos na pagina via jquery*/
+        cy.get('#formNome').invoke('val', 'texto via invoke')
+        cy.window().invoke('alert', 'da pra ver? ')
+        cy.get('#resultado')
+            .invoke('html', '<input type="button" value ="HACKED!">')
+
+    })
+
+
+
 })
