e3761c334d6aa0ff2b926f7807819ff95e7b3e4f
Felipe Meyer <ffmeyer@gmail.com>
2021-04-22 08:03:02 -0300

    foreach nas opcoes do select

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -11,7 +11,7 @@ describe('Dinamic tests', () => {
 
         it(`'Cadastro com comida ${food}'`, () => {
             cy.get('#formNome').type('Usuario')
-            cy.get('#formSobrenome').type('Qualqyer')        
+            cy.get('#formSobrenome').type('Qualquer')        
             cy.get(`[name=formSexo][value='F']`).click()
             cy.xpath(`//label[contains(.,'${food}')]/preceding-sibling::input`).click()
             cy.get('#formEscolaridade').select('Doutorado')
@@ -21,5 +21,21 @@ describe('Dinamic tests', () => {
         })
     })
 
+    it('Deve selecionar comidas menos vegetariano', () => {
+        cy.get('#formNome').type('Usuario')
+        cy.get('#formSobrenome').type('Qualqyer')        
+        cy.get(`[name=formSexo][value='F']`).click()
 
+        cy.get('[name=formComidaFavorita]').each($el => {
+            //clica em todos menos vegetariano
+            if($el.val() != 'vegetariano')
+                cy.wrap($el).click()
+        })
+
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!') 
+    })
 })
+
